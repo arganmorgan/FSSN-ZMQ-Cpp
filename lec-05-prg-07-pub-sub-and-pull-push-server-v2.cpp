11005bd675274b72616ab06b8579296feed4bd5e
arganmorgan <khdhs25@khu.ac.kr>
2025-12-02 22:31:46 +0900

    Add files via upload

---
+++ lec-05-prg-07-pub-sub-and-pull-push-server-v2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <zmq.hpp>
+
+int main() {
+    // context and sockets
+    zmq::context_t ctx(1);
+    zmq::socket_t publisher(ctx, zmq::socket_type::pub);
+    publisher.bind("tcp://*:5557");
+    zmq::socket_t collector(ctx, zmq::socket_type::pull);
+    collector.bind("tcp://*:5558");
+
+    while (true) {
+        zmq::message_t message;
+        collector.recv(message, zmq::recv_flags::none);
+        std::string msg_str(static_cast<char*>(message.data()), message.size());
+        std::cout << "server: publishing update => " << msg_str << std::endl;
+        publisher.send(message, zmq::send_flags::none);
+    }
+}
